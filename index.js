34a03e0c437351013d2bc1f09dd3ef83e037fe24
Devin Wilson <wilson@Devins-Air.attlocal.net>
2020-06-02 16:15:00 -0700

    final stage before now deployment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // write your code here
+const http = require('http');
+
+const server = http.createServer(function(req, res) {
+    console.log('Received' + req.method + 'request for:' +  req.url);
+    res.writeHead(200, {'Content-type': 'text/plain'});
+    res.end('Hello World');
+})
+server.listen(8080, 'localhost', null, function() {
+    console.log('Server is listening in port: 8080')
+});
+
